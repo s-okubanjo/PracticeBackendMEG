44e22131b2406b2bb360021137e75c459a487d14
Samson Okubanjo <samsonokubanjo@outlook.com>
2020-07-03 18:00:34 +0100

    fix eslint errors, added graphql package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
